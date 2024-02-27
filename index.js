a0ad485f9b808e0a0e61013a0ab52f30be1afb86
MarieG41 <mariegond8@gmail.com>
2024-02-27 17:01:18 +0100

    mobile + nav done

---
+++ index.js
@@ -0,0 +1,6 @@
+let hamburgerMenu = document.querySelector('.hamburger');
+let navMobile = document.querySelector('.nav-mobile-shape');
+
+hamburgerMenu.addEventListener('click', function() {
+    navMobile.style.display = "none";
+});
